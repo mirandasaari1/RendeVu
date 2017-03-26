0fd2f6778ee6bf4a742d335306e71c3643ef5702
pomelovanna2 <annapomelov@outlook.com>
2017-03-25 22:24:24 -0700

    Add new chaperone activity and layouts

--- RendeVu/app/src/main/java/edu/csumb/anna/rendevu/MainActivity.java
+++ RendeVu/app/src/main/java/edu/csumb/anna/rendevu/MainActivity.java
@@ -1,7 +1,8 @@
 package edu.csumb.anna.rendevu;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startChaperoneActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, ChaperonesActivity.class);
+        startActivity(intent);
+    }
+
+//    public void startAddDateActivity(View view)
+//    {
+//        Intent intent = new Intent(MainActivity.this, AddDateActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void startPastDatesActivity(View view)
+//    {
+//        Intent intent = new Intent(MainActivity.this, PastDatesActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void startPlannedDatesActivity(View view)
+//    {
+//        Intent intent = new Intent(MainActivity.this, PlannedDatesActivity.class);
+//        startActivity(intent);
+//    }
 }
