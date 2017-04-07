9c85199d58f85514cf9c9b1b5a8f496d9f537164
Miranda Saari <miranda.saari@gmail.com>
2017-04-07 08:18:21 -0700

    add active date files

---
+++ RendeVu/app/src/main/java/edu/csumb/anna/rendevu/ActiveDateActivity.java
@@ -0,0 +1,36 @@
+package edu.csumb.anna.rendevu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Miranda on 4/5/17.
+ */
+
+public class ActiveDateActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_active_date);
+
+            Button endDateButton = (Button) findViewById(R.id.endDateButton);
+            endDateButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+}
+
+
