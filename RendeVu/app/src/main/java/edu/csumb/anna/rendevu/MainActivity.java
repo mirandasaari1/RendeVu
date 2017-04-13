86d82d4430c6bba284c4a77e64dceef52d6193e7
salhernandez <hernandezg.sal@gmail.com>
2017-04-13 00:37:22 -0700

    checks for location permission

--- RendeVu/app/src/main/java/edu/csumb/anna/rendevu/MainActivity.java
+++ RendeVu/app/src/main/java/edu/csumb/anna/rendevu/MainActivity.java
@@ -2,12 +2,16 @@ package edu.csumb.anna.rendevu;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +24,11 @@ import edu.csumb.anna.rendevu.storage.RendeVuDB;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "MainActivity";
+
+
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     String userID = "none";
     @Override
@@ -79,8 +88,119 @@ public class MainActivity extends AppCompatActivity {
         String userID = prefs.getString("userID", "nothing");
 
         toastIt(userID);
-        }
 
+
+        //CHECK FOR PERMISSIONS
+        ///////////////////////////////////
+        //check for internet permission
+        /////////////////////////////////////////////
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.INTERNET);
+        Log.d(TAG, "INTERNET permission " + permissionCheck);
+
+        int permissionCheck3 = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        Log.d(TAG, "FINE location permission " + permissionCheck3);
+
+        // Assume thisActivity is the current activity
+        int permissionCheck2 = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        Log.d(TAG, "COARSE location permission " + permissionCheck2);
+
+        ///////////////////////////////////////////
+
+        //checks if the user has enabled the right permission
+        ////////////////////////////////////////////////////
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            checkPermissions();
+        } else {//if permissions are enabled
+            //GoogleApiClient();
+//            mGoogleApiClient.connect();
+        }
+    }
+
+    public void checkPermissions() {
+        // Assume thisActivity is the current activity
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        Log.d(TAG, "FINE location permission " + permissionCheck);
+
+        // Assume thisActivity is the current activity
+        int permissionCheck2 = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        Log.d(TAG, "COARSE location permission " + permissionCheck2);
+
+        //NEW
+        //ASKS FOR THE PERMISSIONS
+        //////////////////////////////////
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        ////////////////////////////////////////////
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    Log.d(TAG, "permission granted");
+                    //goes to the search portion
+                    //onSearch();
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+//                    mGoogleApiClient.connect();
+//                    mMap.setMyLocationEnabled(true);
+                    //mapLocation();
+
+                }else {
+                    //on permission denied
+                }
+                break;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     @Override
     protected void onResume() {
         super.onResume();
