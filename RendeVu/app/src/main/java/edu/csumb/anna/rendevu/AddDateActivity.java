0f9c1634b6ef8f31630b31c4d8bf8874f020f4b3
Miranda Saari <miranda.saari@gmail.com>
2017-04-24 20:18:05 -0700

    killin' it

--- RendeVu/app/src/main/java/edu/csumb/anna/rendevu/AddDateActivity.java
+++ RendeVu/app/src/main/java/edu/csumb/anna/rendevu/AddDateActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
